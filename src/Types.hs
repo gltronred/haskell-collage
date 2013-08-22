93ee7a3d22cab639636e50eca1e4e6c97319b981
Mansur Ziatdinov <gltronred@gmail.com>
2013-08-22 14:55:53 +0400

    Algortihm typeclass defined

--- src/Types.hs
+++ src/Types.hs
@@ -1,2 +1,22 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Types where
+
+import Data.Word
+
+-- TODO: Change to more appropriate type
+type Coord = (Double, Double)
+type Color = (Word8, Word8, Word8)
+type Point = (Coord, Color)
+type Image = [Point]
+----------------------------------------
+
+class Algorithm where
+  data Region :: *
+  data RegionWithImage :: *
+  toRegions :: Image -> [Region]
+  findSimilar :: [Image] -> Region -> RegionWithImage
+  fromRegions :: [RegionWithImage] -> Image
+
+
 
