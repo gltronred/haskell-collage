58384138a653b7b7e90ce6b58ced2e2dbe8a41bb
Mansur Ziatdinov <gltronred@gmail.com>
2013-08-23 16:09:04 +0400

    File loading ready

--- src/Utils.hs
+++ src/Utils.hs
@@ -7,8 +7,8 @@ loadFile :: FilePath -> IO Image
 loadFile path = let
   extension = takeExtension path
   in case extension of
-    "jpg" -> loadJpegFile path
-    "jpeg" -> loadJpegFile path
-    "png" -> loadPngFile path
-    "gif" -> loadGifFile path
+    ".jpg" -> loadJpegFile path
+    ".jpeg" -> loadJpegFile path
+    ".png" -> loadPngFile path
+    ".gif" -> loadGifFile path
     _ -> error "Unknown file format"
