f126dbbd37f1837234f79e159c3ac04840ad6dee
Yura <yurger2009@gmail.com>
2013-08-23 11:10:26 +0400

    Redefining types
    Changed image type

--- src/Types.hs
+++ src/Types.hs
@@ -3,18 +3,18 @@
 module Types where
 
 import Data.Word
+import Codec.Picture
 
 -- TODO: Change to more appropriate type
 type Coord = (Double, Double)
 type Color = (Word8, Word8, Word8)
 type Point = (Coord, Color)
-type Image = [Point]
 ----------------------------------------
 
 class Algorithm region where
-  toRegions :: Image -> [region]
-  findSimilar :: [Image] -> region -> (region, Int)
-  fromRegions :: [(region,Int)] -> Image
+  toRegions :: DynamicImage -> [region]
+  findSimilar :: [DynamicImage] -> region -> (region, Int)
+  fromRegions :: [(region,Int)] -> DynamicImage
 
 
 
