786adcc80234570a1f40fc2f30b6f2e6b76d41d3
NexentaEL <luaza.armada@mail.ru>
2013-08-28 14:45:43 +0400

    Update ConverterTo64Colour.hs

--- src/ConverterTo64Colour.hs
+++ src/ConverterTo64Colour.hs
@@ -1,24 +1,25 @@
-import Utils
 
-image = loadFile src/img.jpeg
+
+import Control.Monad
+import Utils
 
 -- всё изображение
 converter :: Image -> IO Image
 converter image = do
-	pixelColor <- getPixel image currentPoint -- тип pixelColor - Color
-	setPixel currentPoint (convert pixelColor)
+        (w,h) <- imageSize image
+        forM_ [(i,j)|i<-[0..w], j<-[0..h]] $ \cur -> do
+        	let currentPoint = Point cur
+		pixelColor <- getPixel image currentPoint -- тип pixelColor - Color
+		setPixel currentPoint (convert pixelColor)
 
 -- один пиксель
 convert :: Color -> Color
-convert pixelColor = do
-	pixelColor <- toRGBA pixelColor
-	pixelColor r g b a <- rgb (pixelColor (rounding r rounding g rounding b a))
+convert pixelColor = let
+	(r,g,b,a) = toRGBA pixelColor
+	in rgb (rounding r) (rounding g) (rounding b) a
         
 rounding :: Int -> Int -- или case
 ronding num | num < 43 = 0
 	| num >=43 && num < 128 = 85
 	| num >=128 && num < 213 = 170
 	| num >=213 = 255
-
-currentPoint :: ([Int], [Int])
-currentPoint <- Point ([0..fst Size image], [0..snd Size image])
