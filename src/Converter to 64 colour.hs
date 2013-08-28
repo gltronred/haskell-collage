98e9a94bcacb538c3537042c15b58da99077f267
NexentaEL <luaza.armada@mail.ru>
2013-08-28 13:21:57 +0400

    Simple converter to 64 colour, not reviewed

---
+++ src/Converter to 64 colour.hs
@@ -0,0 +1,24 @@
+import Utils
+
+image = loadFile \src\img.jpeg
+
+-- всё изображение
+converter :: Image -> IO Image
+converter image = do
+	pixelColor <- getPixel image currentPoint -- тип pixelColor - Color
+	setPixel currentPoint (convert pixelColor)
+
+-- один пиксель
+convert :: Color -> Color
+convert pixelColor = do
+	pixelColor = toRGBA pixelColor
+	pixelColor (r g b a) = rgb (pixelColor (rounding r rounding g rounding b a))
+        
+rounding :: Int -> Int -- или case
+ronding num | num < 43 = 0
+	| num >=43 && num < 128 = 85
+	| num >=128 && num < 213 = 170
+	| num >=213 = 255
+
+currentPoint :: ([Int], [Int])
+currentPoint = Point ([0..fst Size image], [0..snd Size image])
