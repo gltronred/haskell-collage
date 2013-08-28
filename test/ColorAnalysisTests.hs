55290ea712432d746e5e5991b0ec1157151b2ea2
Yura <yurger2009@gmail.com>
2013-08-28 16:04:29 +0400

    More tests

--- test/ColorAnalysisTests.hs
+++ test/ColorAnalysisTests.hs
@@ -14,7 +14,8 @@ getColorTests = [ "Pure white" ~: getcolor (rgb 0 0 0) @?= 0
     , "Pure blue with opacity" ~: getcolor (rgba 0 0 255 0) @?= 3
     , "Pure green" ~: getcolor (rgb 0 255 0) @?= 12
     , "Pure green with opacity" ~: getcolor (rgba 0 255 0 0) @?= 12
-    , "Pure red" ~: getcolor (rgba 255 0 0 0) @?= 48
+    , "Pure red" ~: getcolor (rgb 255 0 0) @?= 48
+    , "Pure red with opacity" ~: getcolor (rgba 255 0 0 0) @?= 48
     , "Pure black" ~: getcolor (rgba 255 255 255 0) @?= 63
     , "Dark gray" ~: getcolor (rgba 170 170 170 0) @?= 42
     , "Light gray" ~: getcolor (rgba 85 85 85 0) @?= 21 ]
