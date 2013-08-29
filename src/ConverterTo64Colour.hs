d1bc2358789169959e2b064c1ef5f915bd637f8f
NexentaEL <luaza.armada@mail.ru>
2013-08-29 11:42:19 +0400

    Update ConverterTo64Colour.hs

--- src/ConverterTo64Colour.hs
+++ src/ConverterTo64Colour.hs
@@ -9,7 +9,7 @@ converter image = do
         forM_ [(i,j)|i<-[0..w], j<-[0..h]] $ \cur -> do
         	let currentPoint = cur
 		pixelColor <- getPixel currentPoint image -- тип pixelColor - Color
-		setPixel (image ((convert pixelColor) currentPoint)))
+		setPixel (image ((convert pixelColor) currentPoint))
 
 -- один пиксель
 convert :: Color -> Color
