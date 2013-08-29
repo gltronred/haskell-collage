8d884a307f0dd2705c9209ea6ff6c2978bacda8b
Yura <yurger2009@gmail.com>
2013-08-29 11:03:56 +0400

    fixing

--- test/ColorAnalysisTests.hs
+++ test/ColorAnalysisTests.hs
@@ -6,6 +6,7 @@ import Graphics.GD
 import Utils
 import ColorAnalysis
 
+zeroes=replicate 62 0
 --img = loadFile "test\images\singlecolorimages\000.jpg"
 getColorTests = [ "Pure white" ~: getcolor (rgb 0 0 0) @?= 0
     , "Pure white with opacity" ~: getcolor (rgba 0 0 0 0) @?= 0
@@ -20,7 +21,7 @@ getColorTests = [ "Pure white" ~: getcolor (rgb 0 0 0) @?= 0
     , "Light gray" ~: getcolor (rgba 85 85 85 0) @?= 21 ]
 
     
-makeGraphTests = [ "[63,0,0,63]" ~: makegraph [63,0,0,63] @?= 2:replicate 62 0:2
+makeGraphTests = [ "[63,0,0,63]" ~: makegraph [63,0,0,63] @?= 2:zeroes:2
     , "[0..63]" ~: makegraph [0..63] @?= replicate 64 1
-    , "replicate 100 1" ~: makegraph (replicate 100 1) @?= 0:100:replicate 62 0 ]
+    , "replicate 100 1" ~: makegraph (replicate 100 1) @?= 0:100:zeroes ]
     
