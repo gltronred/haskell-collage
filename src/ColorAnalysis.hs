e3af8da85f64689d63379ae9fde0ef66dc7188f0
YurBurGer <yurger2009@gmail.com>
2013-08-29 13:42:09 +0400

    Finished piece

--- src/ColorAnalysis.hs
+++ src/ColorAnalysis.hs
@@ -6,19 +6,17 @@ import Data.List
 import Algorithm.Square
 --making list of regions
 colorseq::Image->(Point,Point)->IO [RectRegion]
-colorseq img part=do
-    colorlist<-mapM (\(a,b)->getPixel (a,b) img) $ zip [fst$fst$part..fst$snd$part] [snd$fst$part..snd$snd$part]--geting list of colors in picture
+colorseq img ((ulx,uly),(rdx,rdy))=do
+    colorlist<-mapM (\(a,b)->getPixel (a,b) img) $ zip [ulx..rdx] [uly..rdy]--geting list of colors in picture
     let colorlist2=map getcolor colorlist --transforming [Color] to [Int[0..63] ]
     let graph=makegraph colorlist2 --transforming colorlist2 to [Int[0..63]], which is color graph
     let maxcolor=coloranalysis graph
     case maxcolor of
-        Just f->return [RectRegion (fst$part) (snd$part) f]
+        Just f->return [RectRegion (ulx,uly) (rdx,rdy) f]
         Nothing -> do
-            size<-imageSize img
-            let wid=fst size
-            let hei=snd size
-            let halfwid=fst size `div` 2
-            let halfhei=snd size `div` 2
+            (wid,hei)<-imageSize img
+            let halfwid=wid `div` 2
+            let halfhei=hei `div` 2
             let p0=((0,0),(halfwid-1,halfhei-1))
             let p1=((halfwid,0),(wid-1,halfhei-1))
             let p2=((0,halfhei),(halfwid-1,hei-1))
@@ -27,25 +25,24 @@ colorseq img part=do
             if (wid*hei<75*75) 
                 then do
                     color<-averagecolor img
-                    return [RectRegion (fst$part) (snd$part) color]
+                    return [RectRegion (ulx,uly) (rdx,rdy) color]
                 else mapM (colorseq img) [p0,p1,p2,p3] >>= return . concat			
 --transform color to into Int[0..63]
 getcolor::Color->Int
 getcolor col=let
-    get0th (a,_,_,_) = a
-    get1th (_,a,_,_) = a
-    get2th (_,_,a,_) = a
-    crgb=toRGBA col
-    r=div (get0th crgb) 85
-    g=div (get1th crgb) 85
-    b=div (get2th crgb) 85
+    (r1,g1,b1,_)=toRGBA col
+    r=div r1 85
+    g=div g1 85
+    b=div b1 85
     result = (shiftL r 4).|.(shiftL g 2).|.b
     in result
+
 --making graphics of colors in order of colors from 0 to 63
 makegraph::[Int]->[Int]
 makegraph col=let
     result=[length(elemIndices n col)|n<-[0..63]]
     in result
+
 -- analysing graphics,returning color, represented as Int[0..63], or Nothing
 coloranalysis::[Int]->Maybe Color
 coloranalysis col=let
@@ -57,6 +54,7 @@ coloranalysis col=let
     color=snd$maxlist!!0
     result=if null maxlist then Nothing else Just$tocolor color
     in result
+
 --getting average color in image
 averagecolor::Image->IO Color
 averagecolor img=do
@@ -68,6 +66,7 @@ averagecolor img=do
     let pctg=[(floor(fromIntegral(graph!!n)/points*100),n)|n<-[0..63]]
     let result= tocolor(snd(maximum pctg))
     return result
+
 --trasform from Int[0..63] to CInt color
 tocolor::Int->Color
 tocolor col=let
